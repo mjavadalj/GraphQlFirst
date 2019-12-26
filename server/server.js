55d61b1d7c188de8bf26c1bb56854e9fc1a8e6cf
mjavadalj <mjavad.alj@yahoo.com>
2019-12-26 15:51:35 +0330

    deploying on heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-const port = 3000 || process.env.port;
+const port = 3000;
 
 const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port || process.env.port ,()=>{console.log("server is Up")} );
